e1a191c1868c97f96b868787d8862ca92a761604
Douglas Floriano <douglas97floriano@gmail.com>
2026-05-14 18:16:07 -0300

    refatora a estrtura do componente App para incluir conteiner e estilzi a exibicao dos eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css';
 import { Banner } from './components/Banner';
 import { CardEvento } from './components/CardEvento';
@@ -57,16 +57,20 @@ function App() {
             </header>
             <Banner></Banner>
             <FormularioDeEvento temas={temas} aoSubmeter={adicionarEvento} />
-            {temas.map(function (item) {
-                return (
-                    <section key={item.id}>
-                        <Tema tema={item} />
-                        {eventos.map(function (item, index) {
-                            return <CardEvento evento={item} key={index} />;
-                        })}
-                    </section>
-                );
-            })}
+            <section className="container">
+                {temas.map(function (item) {
+                    return (
+                        <section key={item.id}>
+                            <Tema tema={item} />
+                            <div className="eventos">
+                                {eventos.map(function (item, index) {
+                                    return <CardEvento evento={item} key={index} />;
+                                })}
+                            </div>
+                        </section>
+                    );
+                })}
+            </section>
         </main>
     );
 }
